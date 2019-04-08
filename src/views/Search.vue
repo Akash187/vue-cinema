447edd258e6378b9def3d727b1640488327347fd
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-04-09 04:08:11 +0530

    Removed redundant code from search component.

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,7 +2,7 @@
   <div class="main search-favourite-page">
     <div class="title">Search Result</div>
     <div>
-      <Movies :movies="searchedMovies"/>
+      <Movies :movies="this.$store.state.searchedMovies"/>
     </div>
     <div v-show="showError">
       <h1 style="color: red">No movie Found</h1>
@@ -32,16 +32,7 @@
     created(){
       let ref = this;
       if(this.$store.getters.currentPage === 0){
-        ref.$store.dispatch('fetchSearchedMovies').then((movies) => {
-          ref.searchedMovies = movies;
-          if(movies.length === 0){
-            ref.showError = true;
-          }
-        }).catch((error) => {
-          console.log(error);
-        });
-      }else{
-        this.searchedMovies = this.$store.getters.searchedMovies;
+        ref.$store.dispatch('fetchSearchedMovies');
       }
     },
     mounted(){
@@ -51,11 +42,7 @@
         let bottomOfWindow = (document.documentElement.scrollTop + window.innerHeight) === document.documentElement.offsetHeight;
         if (bottomOfWindow && ref.$store.getters.currentPage + 1 <= ref.$store.getters.totalPage && ref.$route.params.query === ref.$store.getters.searchTerm) {
           console.log("Hello");
-          ref.$store.dispatch('fetchSearchedMovies', 'loadMoreMovies').then((movies) => {
-            ref.searchedMovies = movies;
-          }).catch((error) => {
-            console.log(error);
-          });
+          ref.$store.dispatch('fetchSearchedMovies', 'loadMoreMovies');
         }
       };
     }
